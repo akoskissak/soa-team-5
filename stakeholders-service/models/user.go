9212c90d90779aa58ab5ad671e764ff49da60978
davorhoma <davorhoma@gmail.com>
2025-07-20 16:19:30 +0200

    feat: Blocked users cannot login

--- stakeholders-service/models/user.go
+++ stakeholders-service/models/user.go
@@ -16,7 +16,7 @@ type User struct {
 	Email    string				`bson:"email" json:"email"`
 	Password string            `bson:"password,omitempty" json:"password,omitempty"`
 	Role     Role              `bson:"role" json:"role"`
-	IsBlocked bool				`bson:"is_blocked" json:"is_blocked"`
+	IsBlocked bool				`bson:"is_blocked" json:"isBlocked"`
 	
 	Profile UserProfile        `bson:"profile" json:"profile"`
 }
